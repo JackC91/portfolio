492e6c968101c6ca0aacdef2c3596939f058ba61
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-13 15:13:08 +0100

    contact text adjusted to reflect the links below

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     return (
         <div id="contact" className="contact_me">
             <p className="contact_title">contact.</p>
-            <p className="contact_body">Thanks for making it this far! If you're still wanting to know more about me check out my GitHub page, LinkedIn, or contact me via email.</p>
+            <p className="contact_body">Thanks for making it this far! If you're still wanting to know more about me check out my GitHub page, LinkedIn, or contact me via email using the links below.</p>
             <div className="contact_logo"><GitLogo/><LinkedLogo/><EmailLogo/></div>
         </div>
     )

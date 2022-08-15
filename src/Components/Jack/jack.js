d4482898f9ba6a380dc315673d9e632e3d9d1a46
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-15 22:13:07 +0100

    typing effect added to first name

--- src/Components/Jack/jack.js
+++ src/Components/Jack/jack.js
@@ -1,3 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+
 export default function Jack() {
-  return <p className="jack">jack</p>;
+  const [text, setText] = useState("")
+  const [fullText, setFullText] = useState("jack")
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    if (index < fullText.length) {
+      setTimeout(() => {
+        setText(text + fullText[index])
+        setIndex(index + 1)
+      }, 400)
+    }
+  }, [index])
+
+  return <p className="jack">{text}</p>;
 }

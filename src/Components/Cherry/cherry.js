ee57eaa21fb219d56e6ca009009056522b631472
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-15 22:30:52 +0100

    type effect added to surname but timing needs working on

--- src/Components/Cherry/cherry.js
+++ src/Components/Cherry/cherry.js
@@ -1,3 +1,18 @@
+import React, { useState, useEffect } from "react";
+
 export default function Cherry() {
-    return <p className="cherry">cherry.</p>;
+  const [text, setText] = useState("")
+  const [fullText, setFullText] = useState("cherry.")
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    if (index < fullText.length) {
+      setTimeout(() => {
+        setText(text + fullText[index])
+        setIndex(index + 1)
+      }, 400)
+    }
+  }, [index])
+
+    return <p className="cherry">{text}</p>;
   }

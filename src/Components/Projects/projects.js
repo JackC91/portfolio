3f641ac9c277332e8920aa5fb468079f62cd3f3a
JackC91 <jackjcherry@hotmail.co.uk>
2022-08-21 12:34:59 +0100

    project image and text updated

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -5,13 +5,15 @@ const text =
 
 const weekNineText = "A topic tracking and proficiency app that was developed by a team of four bootcampers over the course of a week with School of Code, the image above will take you to the deployed app, both front and back end repos can be accessed via my GitHub page.";
 
+const weekTwelveText = "As part of a team of week this app was built over the course of four weeks for our final project in the School of Code Bootcamp 12. The picture above will take you to the fully deployed version of the app, the fron and back end repos can be found on my personal GitHub account."
+
 export default function Projects() {
   return (
     <div id="project">
       <p className="project_title">projects.</p>
       <div className="projects">
         <ProjectCard text={weekNineText} src={require("../../wk9.png")} alt="screenshot of week 9 app" />
-        <ProjectCard text={text} src={require("../../Care-full.png")} alt="screenshot of week 12 app"/>
+        <ProjectCard text={weekTwelveText} src={require("../../Care-full.png")} alt="screenshot of week 12 app"/>
         <ProjectCard text={text} />
       </div>
     </div>

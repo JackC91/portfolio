3ca93df9894b017b6eb38130cca443abe50063bc
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-07 13:18:16 +0100

    cocktail project card loaded to portfolio

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -4,6 +4,8 @@ const weekNineText = "A topic tracking and proficiency app that was developed by
 
 const weekTwelveText = "As part of a team of six this app was built over the course of four weeks for our final project in the School of Code Bootcamp 12. The picture above will take you to the fully deployed version of the app, the front and back end repos can be found on my personal GitHub account."
 
+const cocktailsText = "This is a solo project I built to cement my knowledge and abilities in React and CSS without the safety net of pair programming. While this project meets the MVP of what I intended it to I still consider it a WIP to be added to as my knowledge of the technologies grows.";
+
 export default function Projects() {
   return (
     <div id="project">
@@ -11,6 +13,7 @@ export default function Projects() {
       <div className="projects">
         <ProjectCard text={weekNineText} href={process.env.REACT_APP_WEEK_NINE} src={require("../../wk9.png")} alt="screenshot of week 9 app" />
         <ProjectCard text={weekTwelveText} href={process.env.REACT_APP_CARE_FULL} src={require("../../Care-full.png")} alt="screenshot of week 12 app"/>
+        <ProjectCard text={cocktailsText} href={process.env.REACT_APP_COCKTAILS} src={require("../../cocktails.png")} alt="screenshot of Cocktails For You app"/>
       </div>
     </div>
   );
